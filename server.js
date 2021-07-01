8a42920d31d7602939508e6b2d757c533d7a954e
RubenDeBeer <rubendebeercoding@gmail.com>
2021-07-01 10:30:50 +0200

    Database model

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+
+mongoose.connect('mongodb://localhost/urShortner',{
+    useNewUrlParser:true , useUnifiedTopology:true
+})
 
 app.set('view engine','ejs')
 //Event listener
@@ -7,6 +12,10 @@ app.get('/',(req,res)=>{
     res.render('index')
 })
 
-app.listen(process.env.PORT || 5000);
+app.post('/shortUrls',(req,res)=>{
+
+})
+
+app.listen(process.env.PORT || 5000) 
 
 
